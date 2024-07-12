5a0d63caf539798c154b5d5b8c5578847c607c1d
Flávia Souza <flavia.souza.lvi@gmail.com>
2024-07-12 20:49:06 -0300

    Going up Integer

---
+++ Review4_Integer.php
@@ -0,0 +1,15 @@
+<?php
+
+// Integers são todos os números negativos e positivos
+// No PHP pode ser integer (números menores) e Long (números maiores)
+
+$number = 10;
+$x = -50;
+
+var_dump ($number);
+var_dump ($x);
+
+// Observação1.: Todas as variáveis não carregam acentos
+
+
+?>
