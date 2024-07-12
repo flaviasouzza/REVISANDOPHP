d9f9d3f2eeb52578f3d9f09932a7ddec5dc47ae2
Flávia Souza <flavia.souza.lvi@gmail.com>
2024-07-12 20:15:48 -0300

    Going up Constantes

---
+++ Review2_Constante.php
@@ -0,0 +1,13 @@
+<?php
+// Constante é um recurso da linguagem de programaão que tem como objetivo armazenar um valor em memória de modo que não possa ser alterado 
+
+// Vamos continuar no exemplo entregador de delivery 
+
+
+    define("valorDia", "80");
+    define("gorjetas", "40");
+
+    echo valorDia + gorjetas;
+    
+?>
+
