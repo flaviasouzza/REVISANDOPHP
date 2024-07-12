dbeb6713d4468df3aadda243b28922d7d2e783a0
Flávia Souza <flavia.souza.lvi@gmail.com>
2024-07-12 20:16:15 -0300

    Going up String

---
+++ Review3_String.php
@@ -0,0 +1,26 @@
+<?php
+
+// String é uma sequência de caracteres, que em geral é utilizada para representar palavras, frases, ou textos de um porgrama. 
+// Lembrando que uma string não armazena só texto, também armazena variáveis e números entre aspas duplas;
+
+
+    $s = "Curso de PHP\n";
+    echo $s;
+
+    echo "Curso tipos de variáveis em PHP \n";
+
+    $texto = "<h1>Texto H1 </h1> \n";
+    $h3 = "<h3> Texto H3 </h3> \n";
+    $h4 = "<h4> Texto H4 </h4> \n";
+    $h5 = "<h5> Texto H5 </h5> \n";
+
+    echo $texto;
+    echo $h3;
+    echo $h4;
+    echo $h5;
+   
+
+
+// Observação1.: <h1>, <h2>, <h3>, <h4>, <5> São provenientes da linguagem HTML, que muitas vezes podemos utilizar o código junto ao do PHP
+
+?>
